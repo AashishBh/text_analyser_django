ecb6a792014f4805f66099c681ff6a59aef90eb7
AashishBh <aaceesh.bhandary@gmail.com>
2022-01-05 09:20:34 +0545

    created one.txt

--- textutils/views.py
+++ textutils/views.py
@@ -1,7 +1,13 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("hello")
+    return HttpResponse("hello <h1>World </h1>")
 
 def about(request):
     return HttpResponse("about page")
+
+def one(request):
+    f = open("../textutils/one.txt", "r")
+    file_content = f.read()
+    f.close()
+    return HttpResponse(file_content, content_type="text/plain")
