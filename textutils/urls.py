0a593b6f48216a91c24cc8b05da351fd675e68b0
AashishBh <aaceesh.bhandary@gmail.com>
2022-01-07 13:54:05 +0545

    created analyser.html

--- textutils/urls.py
+++ textutils/urls.py
@@ -15,11 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from . import  views
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name="index"),
     path('about', views.about, name="about"),
-    path('one', views.one, name="one")
+    path('file', views.file, name="file"),
+    path('navigator', views.navigator, name="navigator"),
+    path('analyser', views.analyser, name="analyser")
 ]
--- textutils/views.py
+++ textutils/views.py
@@ -1,13 +1,27 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("hello <h1>World </h1>")
+    return render(request, 'index.html')
+    # return HttpResponse("hello <h1>World </h1>")
 
 def about(request):
     return HttpResponse("about page")
 
-def one(request):
-    f = open("../textutils/one.txt", "r")
-    file_content = f.read()
-    f.close()
-    return HttpResponse(file_content, content_type="text/plain")
+def analyser(request):
+
+    return render(request,'analyser')
+
+def file(request):
+    f = open("textutils/one.txt", "r")
+    return HttpResponse(f.read())
+
+def navigator(request):
+    return HttpResponse('''
+    <h1>My Personal Navigator </h1>
+    <p> <a href = "https://www.facebook.com">Facebook </a></p>
+    <p> <a href = "https://www.instagram.com">Instagram </p>
+    <p> <a href = "https://www.twitter.com">Twitter </p>
+    <p> <a href = "https://www.reddit.com">Reddit </p>
+    <p> <a href = "https://www.telegram.org">Telegram </p>
+    ''')
